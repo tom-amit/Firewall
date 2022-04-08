9ead7f46891cf0a5e10fd6cd54cbd6746ce609ca
Tom Kark <karktom@gmail.com>
2022-04-09 00:32:18 +0300

    Removed pcap++ directory from project dir.
    Most importantly converted the whole project from Makefile to CMake...

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 // Created by tk on 30/03/2022.
 //
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
 #include "include/Controller.h"
 using std::string;
 
-int main() {
+int main(int argc, char *argv[]) {
     Controller c = Controller();
     c.run();
 }
