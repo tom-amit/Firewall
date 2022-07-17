8cb22d22c41046e44eefd777b58ef7c3fd8bf84e
tom <karktom@gmail.com>
2022-07-17 18:37:01 +0300

    frontend NICS changes

--- assets/scripts/angular_utils.js
+++ assets/scripts/angular_utils.js
@@ -1,23 +1,53 @@
+let nic1  = undefined
+let nic2 = undefined
+$(document).ready(function() {
+    angular.element(document.getElementById('choose_nics')).scope().select();
+});
 angular
     .module("FW", ["ngMaterial", "ngMessages", "material.svgAssetsCache"])
 
     .controller("NICSModal", function ($scope, $mdDialog) {
         //use later for nics variables
         // $scope.status = "  ";
-        $scope.NICS = ["1", '2'];
+        //the template URL has ng-repeat for each nic
+        $scope.nics = [
+            {
+                name: "eth0"
+            },
+            {
+                name: "eth1"
+            }
+        ];
+        $scope.select1 = undefined;
+        $scope.select2 = undefined;
+
+        $scope.changeClose = function(){
+            console.log("1:" + ( $scope.select1 === undefined) + "2:" + ($scope.select2 === undefined) + "3:" + ($scope.select1 === $scope.select2));
+            $("#close").attr("disabled", (
+               $scope.select1 === undefined ||  $scope.select2 === undefined || $scope.select1 === $scope.select2
+            ));
+            nic1 = $scope.select1;
+            nic2 = $scope.select2;
+        };
         $scope.select = function() {
             $mdDialog.show({
-               //template to have 2 dropdowns for nics
+                escapeToClose: false,
+                clickOutsideToClose: false,
+                backdropClickToClose: false,
                 templateUrl: 'scripts/templates/nics_modal.html',
                 controller: function DialogController($scope, $mdDialog) {
                     $scope.closeDialog = function() {
                         $mdDialog.hide();
                     }
+                },
+                onComplete: function(){
+                    nic1 = undefined;
+                    nic2 = undefined;
+                    $scope.changeClose();
                 }
             }).then(function() {
-                alert("confirmation");
-            }, function() {
-                alert("annulation");
+                $("#nic_1").text("First Network Card: " + nic1.name);
+                $("#nic_2").text("Second Network Card: " + nic2.name);
             });
-    }
-});
+        }
+    });
